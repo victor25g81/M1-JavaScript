877f0cb08b7634aaa749d35f251969a6a0077052
victor25g81 <100184175+victor25g81@users.noreply.github.com>
2023-02-22 09:37:09 -0600

    Nota acerca de var

---
+++ P01-Variables/ex03_imprimir_edad_nombre.js
@@ -0,0 +1,15 @@
+// Ex03: Crear una función que imprima la edad y el nombre de una persona.
+// Se deben crear parámetros para recibir eos dos valores.
+// Las variables deben ser creadas con let o var.
+
+function edad_nombre(nombre, edad){
+    console.log('Su nombre es:', nombre);
+    console.log('Su edad es:', edad);
+}
+
+let nombre = 'victor';
+let edad = '40';
+
+edad_nombre(nombre, edad);
+
+// NOTA: Úsese let mientras que NO sea necesario utilizar var.
