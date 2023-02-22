d741406f00daaf7f91a12df34d62b4b20efb3a15
victor25g81 <100184175+victor25g81@users.noreply.github.com>
2023-02-22 09:04:55 -0600

    Uso de var, let, y const para crear variables

---
+++ P01-Variables/ex01_intro.js
@@ -0,0 +1,31 @@
+// ¿Cómo crear variables en JS?
+
+// 1. Utilizar var:
+
+var nombre = 'Víctor';
+var apellido = 'De la fuente';
+var email = 'victor@mail.co';
+
+console.log('Nombre: ', nombre);
+console.log('Apellido: ', apellido);
+console.log('Email: ', email);
+
+// 2. Utilizar let:
+
+let profesion = 'Programador';
+let cantidadHijos = 1;
+let paisResidencia = 'México';
+
+console.log('Profesión:', profesion);
+console.log('Apellido:', apellido);
+console.log('País residencia:', paisResidencia);
+
+// 3. Crear constantes:
+
+const paisNacimiento = 'México';
+const cantidadHermanos = 4;
+
+console.log('País nacimiento:', paisNacimiento);
+console.log('Cantidad hermanos:', cantidadHermanos);
+
+// paisNacimiento = 'Italia';
